bcad7b5a9b0bb6f8410c6337350b3d4906fe0492
ThongND <thongndhe170758@fpt.edu.vn>
2024-10-07 19:56:01 +0700

    update layout + token model

--- TCViettetlFC_Client/obj/Debug/net8.0/TCViettetlFC_Client.AssemblyInfo.cs
+++ TCViettetlFC_Client/obj/Debug/net8.0/TCViettetlFC_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87c57cb46d0b339e77d8669c112084aee0ddd51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f65a132a4ca1a38502481002ae95134c39642b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
