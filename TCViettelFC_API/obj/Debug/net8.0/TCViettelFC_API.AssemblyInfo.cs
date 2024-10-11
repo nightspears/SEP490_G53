cb5d7f5899290abe7c9354fa28650f899a8b253a
ThongND <thongndhe170758@fpt.edu.vn>
2024-10-11 16:56:38 +0700

    fix important js ref

--- TCViettelFC_API/obj/Debug/net8.0/TCViettelFC_API.AssemblyInfo.cs
+++ TCViettelFC_API/obj/Debug/net8.0/TCViettelFC_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCViettelFC_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce28ab9de190c79b9f4521a9e88b38d16ef894f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38420fad0511c6c9a04179b0ba6ae4b431af7978")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCViettelFC_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCViettelFC_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
