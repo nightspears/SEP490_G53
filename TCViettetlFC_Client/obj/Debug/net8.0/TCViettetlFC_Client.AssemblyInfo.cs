15d1f3b7ee393e36cdd3106e86e2c8dc9a1eb0ef
Nguyen Quy Duong <duongnqhe170786@fpt.edu.vn>
2024-11-02 16:03:36 +0700

    optimize shipment detail, order product management and order product detail display

--- TCViettetlFC_Client/obj/Debug/net8.0/TCViettetlFC_Client.AssemblyInfo.cs
+++ TCViettetlFC_Client/obj/Debug/net8.0/TCViettetlFC_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa76f5db32478821508b1deccdae09306045eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9def46ece2c93852fc45e2cae640cb93529086")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCViettetlFC_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
