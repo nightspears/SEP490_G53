a58aaf0dc7492b824f01b26b30bec0f6a6c962d2
nightspears <antrungdh@gmail.com>
2024-11-27 13:59:48 +0700

    chỉnh một số thứ ở trang home

--- TCViettelFC_API/Models/Sep490G53Context.cs
+++ TCViettelFC_API/Models/Sep490G53Context.cs
@@ -70,29 +70,29 @@ public partial class Sep490G53Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        // Kiểm tra nếu chưa được cấu hình
-        if (!optionsBuilder.IsConfigured)
-        {
-            // Sử dụng biến môi trường để chọn provider
-            var isTesting = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Testing";
-
-            if (isTesting)
-            {
-                optionsBuilder
-                    .UseInMemoryDatabase("TestDatabase")
-                    .ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning));
-            }
-            else
-            {
-                var config = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .Build();
-
-                optionsBuilder.UseSqlServer(config.GetConnectionString("value"));
-            }
-        }
-
-        base.OnConfiguring(optionsBuilder);
+    //    // Kiểm tra nếu chưa được cấu hình
+    //    if (!optionsBuilder.IsConfigured)
+    //    {
+    //        // Sử dụng biến môi trường để chọn provider
+    //        var isTesting = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Testing";
+    //
+    //        if (isTesting)
+    //        {
+    //            optionsBuilder
+    //                .UseInMemoryDatabase("TestDatabase")
+    //                .ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+    //        }
+    //        else
+    //        {
+    //            var config = new ConfigurationBuilder()
+    //                .AddJsonFile("appsettings.json")
+    //                .Build();
+    //
+    //            optionsBuilder.UseSqlServer(config.GetConnectionString("value"));
+    //        }
+    //    }
+    //
+    //    base.OnConfiguring(optionsBuilder);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
