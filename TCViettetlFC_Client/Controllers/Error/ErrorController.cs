e28c545a402e5d71bc187bb4a5cb613bf9fedfcc
Nguyễn Quốc Tuấn <129683215+TunnTapCode@users.noreply.github.com>
2024-12-04 23:05:06 +0700

    add error page

---
+++ TCViettetlFC_Client/Controllers/Error/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCViettetlFC_Client.Controllers.Error
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/404")]
+        public IActionResult Error404()
+        {
+            return View();
+        }
+
+        [Route("Error/403")]
+        public IActionResult Error403()
+        {
+            return View();
+        }
+
+        [Route("Error/400")]
+        public IActionResult Error400()
+        {
+            return View();
+        }
+    }
+}
